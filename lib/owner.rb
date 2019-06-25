61cc8b01e50d9898fb9ca6923fa01ea541f24073
James Britton <brittonjp@gmail.com>
2019-06-25 06:36:28 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 class Owner
   
   attr_accessor :name, :pets
@@ -10,47 +8,8 @@ class Owner
   def initialize(name)
     @name = name
     @species = "human"
-    @pets = { fishes: [], cats: [], dogs: [] }
     @@all << self
-  end
-  
-  def buy_fish(fish)
-    @pets[:fishes] << Fish.new(fish)
-  end
-  
-  def buy_cat(cat)
-    @pets[:cats] << Cat.new(cat)
-  end
-  
-  def buy_dog(dog)
-    @pets[:dogs] << Dog.new(dog)
-  end
-  
-  def walk_dogs
-    @pets[:dogs].each { |dog| dog.mood = "happy" }
-  end
-  
-  def play_with_cats
-    @pets[:cats].each { |cat| cat.mood = "happy" }
-  end
-  
-  def feed_fish
-    @pets[:fishes].each { |fish| fish.mood = "happy" }
-  end
-  
-  def sell_pets
-    @pets.each_value do |animal_array|
-      animal_array.each { |animal| animal.mood = "nervous" }
-    end
-    @pets.clear
-  end
-  
-  def list_pets
-    "I have #{@pets[:fishes].count} fish, #{@pets[:dogs].count} dog(s), and #{@pets[:cats].count} cat(s)."
-  end
-  
-  def say_species
-    "I am a #{@species}."
+    @pets = {  }
   end
   
   def self.all
@@ -63,6 +22,10 @@ class Owner
   
   def self.reset_all
     @@all.clear
+  end
+  
+  def say_species
+    "I am a human."
   end
   
 end
